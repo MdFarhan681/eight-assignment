f1696beb28d02234f8f0e4efea557b87536cbc71
MdFarhan681 <ishrakemon681@gmail.com>
2025-10-11 13:10:59 +0600

    sixth

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,40 +8,38 @@ import Installed from "../pages/Installed/Installed";
 import Details from "../pages/Details/Details";
 import NotFound from "../pages/NotFound/NotFound";
 
-
-
-
-
- export const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
-    Component:Root,
-    errorElement:<Error></Error>,
-    children:[
-        {
-            index:true,
-            loader:()=>fetch('/AppData.json'),
-            path:"/",
-            Component:Home
-        },{
-          
-          path:"/AllApps",
-          Component:AllApps,
-          loader:()=>fetch('/AppData.json')
-        },
-        {
-          path:"/Installed",
-          Component:Installed,
-          loader:()=>fetch('/AppData.json')
-        },{
-          path:"/Details/:id",
-          Component:Details,
-          errorElement:<NotFound></NotFound>,
-          loader:()=>fetch('/AppData.json'),
-        },{
-          path:"/not-found",
-          Component: NotFound
-        }
-    ]
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        loader: () => fetch("/AppData.json"),
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/AllApps",
+        Component: AllApps,
+        loader: () => fetch("/AppData.json"),
+      },
+      {
+        path: "/Installed",
+        Component: Installed,
+        loader: () => fetch("/AppData.json"),
+      },
+      {
+        path: "/Details/:id",
+        Component: Details,
+        errorElement: <NotFound></NotFound>,
+        loader: () => fetch("/AppData.json"),
+      },
+      {
+        path: "/not-found",
+        Component: NotFound,
+      },
+    ],
   },
 ]);
